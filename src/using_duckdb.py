0a3145497c2e617256787f6340baa0c58b51736f
Nicolas Doege <github.repsu@passinbox.com>
2025-01-04 14:35:36 -0300

    reformatting files with black

--- src/using_duckdb.py
+++ src/using_duckdb.py
@@ -1,8 +1,10 @@
 import time
 import duckdb
 
+
 def create_duckdb(file_path: str) -> None:
-  duckdb.sql(f"""
+    duckdb.sql(
+        f"""
     SELECT station, 
       MIN(temperature) AS min_temperature, 
       CAST(AVG(temperature) AS DECIMAL(3,1)) AS avg_temperature, 
@@ -10,16 +12,17 @@ def create_duckdb(file_path: str) -> None:
     FROM read_csv('{file_path}', auto_detect=False, sep=';', columns={{'station': 'VARCHAR', 'temperature': 'DECIMAL(3,1)'}})
     GROUP BY station
     ORDER BY station
-  """).show()
+  """
+    ).show()
 
 
 if __name__ == "__main__":
-  data_path: str = "../data/weather_measurements.txt"
+    data_path: str = "../data/weather_measurements.txt"
 
-  print("Starting file processing")
+    print("Starting file processing")
 
-  start_time: float = time.time()
-  create_duckdb(file_path=data_path)
-  end_time: float = time.time()
-  
-  print(f"\nProcessing completed in {end_time - start_time:.2f} seconds.")
+    start_time: float = time.time()
+    create_duckdb(file_path=data_path)
+    end_time: float = time.time()
+
+    print(f"\nProcessing completed in {end_time - start_time:.2f} seconds.")
--- src/using_polars.py
+++ src/using_polars.py
@@ -1,29 +1,40 @@
 import time
 import polars as pl
 
-def create_pl_df(file_path: str) -> pl.DataFrame:
-  pl.Config.set_streaming_chunk_size(4_000_000)
 
-  pl_df: pl.DataFrame = pl.scan_csv(file_path, separator=";", has_header=False, 
-                      new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
-  
-  pl_df_agg: pl.DataFrame = pl_df.group_by("station").agg(
-                max = pl.col("measure").max(),
-                min = pl.col("measure").min(), 
-                mean = pl.col("measure").mean()
-              ).sort("station").collect(streaming=True)
-  
-  return pl_df_agg
+def create_pl_df(file_path: str) -> pl.DataFrame:
+    pl.Config.set_streaming_chunk_size(4_000_000)
+
+    pl_df: pl.DataFrame = pl.scan_csv(
+        file_path,
+        separator=";",
+        has_header=False,
+        new_columns=["station", "measure"],
+        schema={"station": pl.String, "measure": pl.Float64},
+    )
+
+    pl_df_agg: pl.DataFrame = (
+        pl_df.group_by("station")
+        .agg(
+            max=pl.col("measure").max(),
+            min=pl.col("measure").min(),
+            mean=pl.col("measure").mean(),
+        )
+        .sort("station")
+        .collect(streaming=True)
+    )
+
+    return pl_df_agg
 
 
 if __name__ == "__main__":
-  data_path: str = "../data/weather_measurements.txt"
+    data_path: str = "../data/weather_measurements.txt"
 
-  print("Starting file processing")
+    print("Starting file processing")
 
-  start_time: float = time.time()
-  df: pl.DataFrame = create_pl_df(file_path=data_path)
-  end_time: float = time.time()
-  
-  print(df.head())
-  print(f"\nProcessing completed in {end_time - start_time:.2f} seconds.")
+    start_time: float = time.time()
+    df: pl.DataFrame = create_pl_df(file_path=data_path)
+    end_time: float = time.time()
+
+    print(df.head())
+    print(f"\nProcessing completed in {end_time - start_time:.2f} seconds.")
